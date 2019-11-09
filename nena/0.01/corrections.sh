17d6f891414f877404db7edc6b328b63dbb51db7
Cody Kingham <codyakingham@gmail.com>
2019-11-09 03:52:02 +0000

    Add missing <R> tag correction

--- nena/0.01/corrections.sh
+++ nena/0.01/corrections.sh
@@ -4,3 +4,7 @@
 MEASUREFORME="Barwar/Measure For Measure.nena"
 echo "Adding missing line number in [$MEASUREFORME]"
 sed -i "" "8 s/^/(1) /" "$MEASUREFORME"
+
+PRINCESSAD="Urmi_C/The Adventures Of A Princess.nena"
+echo "Adding missing <R> tag to [$PRINCESSAD]"
+sed -i "" "s/\(ʾe-<R>\*buk̭ḗṱ\*\)/\1<R>/" "$PRINCESSAD"
