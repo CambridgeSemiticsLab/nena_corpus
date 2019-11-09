373bda0b252aa3b27eed85b37baf03a32565dc04
Cody Kingham <codyakingham@gmail.com>
2019-11-09 03:55:04 +0000

    Flatten file variable

--- nena/0.01/corrections.sh
+++ nena/0.01/corrections.sh
@@ -1,10 +1,11 @@
 # Corrections to Source Data
 
 # Add missing line number 
-MEASUREFORME="Barwar/Measure For Measure.nena"
-echo "Adding missing line number in [$MEASUREFORME]"
-sed -i "" "8 s/^/(1) /" "$MEASUREFORME"
+FILE="Barwar/Measure For Measure.nena"
+echo "Adding missing line number in [$FILE]"
+sed -i "" "8 s/^/(1) /" "$FILE"
 
-PRINCESSAD="Urmi_C/The Adventures Of A Princess.nena"
-echo "Adding missing <R> tag to [$PRINCESSAD]"
-sed -i "" "s/\(ʾe-<R>\*buk̭ḗṱ\*\)/\1<R>/" "$PRINCESSAD"
+# Add missing <R> foreign language tag
+FILE="Urmi_C/The Adventures Of A Princess.nena"
+echo "Adding missing <R> tag to [$FILE]"
+sed -i "" "s/\(ʾe-<R>\*buk̭ḗṱ\*\)/\1<R>/" "$FILE"
