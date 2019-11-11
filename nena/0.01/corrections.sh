0c6ea0d341fd4320d9d6de4143b603cb4f94c606
Cody Kingham <codyakingham@gmail.com>
2019-11-11 20:17:55 +0000

    clean up titles

--- nena/0.01/corrections.sh
+++ nena/0.01/corrections.sh
@@ -1,16 +1,16 @@
 # Corrections to Source Data
 
 # Add missing line number 
-FILE="Barwar/Measure For Measure.nena"
+FILE="Barwar/Measure for Measure.nena"
 echo "Adding missing line number in [$FILE]"
 sed -i "" "8 s/^/(1) /" "$FILE"
 
 # Add missing <R> foreign language tag
-FILE="Urmi_C/The Adventures Of A Princess.nena"
+FILE="Urmi_C/The Adventures of a Princess.nena"
 echo "Adding missing <R> tag to [$FILE]"
 sed -i "" "s/\(ʾe-<R>\*buk̭ḗṱ\*\)/\1<R>/" "$FILE"
 
 # Re-number the lines for a split text
-FILE="Urmi_C/The Wife Who Learns How To Work (2).nena"
+FILE="Urmi_C/The Wife Who Learns How to Work (2).nena"
 echo "Renumbering lines in $FILE"
 python3 reline.py "$FILE"
