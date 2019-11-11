33a12701bda42e3027516dd77428c81b15d791dc
Cody Kingham <codyakingham@gmail.com>
2019-11-11 12:11:37 +0000

    Add line corrections to split work

--- nena/0.01/corrections.sh
+++ nena/0.01/corrections.sh
@@ -9,3 +9,8 @@ sed -i "" "8 s/^/(1) /" "$FILE"
 FILE="Urmi_C/The Adventures Of A Princess.nena"
 echo "Adding missing <R> tag to [$FILE]"
 sed -i "" "s/\(ʾe-<R>\*buk̭ḗṱ\*\)/\1<R>/" "$FILE"
+
+# Re-number the lines for a split text
+FILE="Urmi_C/The Wife Who Learns How To Work (2).nena"
+echo "Renumbering lines in $FILE"
+python3 reline.py "$FILE"
