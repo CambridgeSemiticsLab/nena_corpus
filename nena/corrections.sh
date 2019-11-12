9933c39578d6a7bd5650e40a1eeb31074bf7ff75
Cody Kingham <codyakingham@gmail.com>
2019-11-12 18:03:52 +0000

    Move corrections out of dialects dir

--- nena/corrections.sh
+++ nena/corrections.sh
@@ -1,34 +1,36 @@
 # Corrections to Source Data
 
+VERS="0.01"
+
 # Add missing line number 
-FILE="Barwar/Measure for Measure.nena"
+FILE="$VERS/Barwar/Measure for Measure.nena"
 echo "Adding missing line number in [$FILE]"
 sed -i "" "8 s/^/(1) /" "$FILE"
 
 # Add missing <R> foreign language tag
-FILE="Urmi_C/The Adventures of a Princess.nena"
+FILE="$VERS/Urmi_C/The Adventures of a Princess.nena"
 echo "Adding missing <R> tag to [$FILE]"
 sed -i "" "s/\(ʾe-<R>\*buk̭ḗṱ\*\)/\1<R>/" "$FILE"
 
 # Re-number the lines for a split text
-FILE="Urmi_C/The Wife Who Learns How to Work (2).nena"
+FILE="$VERS/Urmi_C/The Wife Who Learns How to Work (2).nena"
 echo "Renumbering lines in $FILE"
 python3 reline.py "$FILE"
 
 # add missing prosaic boundaries
 SUBPROSA="s/\([ /]*$\)/ˈ\1/" 
-FILE="Barwar/Qaṭina Rescues His Nephew From Leliθa.nena"
+FILE="$VERS/Barwar/Qaṭina Rescues His Nephew From Leliθa.nena"
 echo "Fixing missing prosody boundaries in $FILE"
 sed -i "" "22,26 $SUBPROSA" "$FILE"
 
-FILE="Barwar/The Battle With Yuwanəs the Armenian.nena"
+FILE="$VERS/Barwar/The Battle With Yuwanəs the Armenian.nena"
 echo "Fixing missing prosody boundaries in $FILE"
 sed -i "" "18,21 $SUBPROSA" "$FILE"
 sed -i "" "65,67 $SUBPROSA" "$FILE"
 sed -i "" "69,73 $SUBPROSA" "$FILE"
 sed -i "" "86,87 $SUBPROSA" "$FILE"
 
-FILE="Barwar/The Sisisambər Plant.nena"
+FILE="$VERS/Barwar/The Sisisambər Plant.nena"
 echo "Fixing missing prosody boundaries in $FILE"
 sed -i "" "12,18 $SUBPROSA" "$FILE"
 sed -i "" "30,33 $SUBPROSA" "$FILE"
