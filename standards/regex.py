ea92dd32a20cd325646e79fc2bdec64a47b6f5e1
Cody Kingham <codyakingham@gmail.com>
2020-04-21 17:44:48 +0100

    Correct name

--- standards/regex.py
+++ standards/regex.py
@@ -1,4 +1,4 @@
-# version 0.01 beta
+# version 0.02 beta
 
 import unicodedata
 
@@ -20,10 +20,12 @@ def normalize_char(char):
     """
     return unicodedata.normalize('NFD', char).lower()
 
-# Regex for foreign characters wrapped with <Lang>Some foreign chars<Lang>
+# Regex for foreign letters wrapped with a language tag
+#   e.g. <P>Some foreign letters<P>
+# CAUTION: does not yet exclude puntuators
 foreign_chars = '.[\u0300-\u036F]*'
 
-# Regex for NENA Characters
+# Regex for NENA letters
 base_chars = '[a-zðɟəɛʾʿθ]'
 unaccented = base_chars + '(?![\u0300-\u036F])'
 cons_accented = '[dhlmprstzð]\u0323|[ckpt]\u032d|[csz]\u030c|c[\u0323\u032d]\u030c|g\u0307'
